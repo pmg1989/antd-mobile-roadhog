1c37ed14e63d86bd3fdb6e038b4288551ba76ccc
pmg1989 <972401854@qq.com>
2018-03-14 14:05:41 +0800

    fix svg

--- src/containers/Demo.js
+++ src/containers/Demo.js
@@ -1,15 +1,33 @@
 import React from 'react'
+import { Header, Icon } from 'components'
+import { Button, WhiteSpace, WingBlank } from 'antd-mobile'
 import styles from './Demo.less'
 
 const Demo = () => {
   return (
-    <div className={styles.box}>
-      <span>
-        i am demo
-      </span>
-      <span>
-        i am demo
-      </span>
+    <div className="content-box">
+      <Header>todo mvc</Header>
+      <div className="content">
+        <WingBlank>
+          <div className={styles.box}>
+            <span className={styles.title}>
+              i am demo title
+            </span>
+          </div>
+          <div className={styles.box}>
+            <span>i am demo 1</span>
+            <span>i am demo 2</span>
+          </div>
+          <Button type="primary" disabled>
+            <Icon type={require('svg/loading.svg')} /> default disabled
+          </Button><WhiteSpace />
+          <Button type="primary">primary</Button><WhiteSpace />
+          <Button type="warning">warning</Button><WhiteSpace />
+          <Button loading>loading button</Button><WhiteSpace />
+          <Button icon="check-circle-o">with icon</Button><WhiteSpace />
+          <Button icon={<img src="https://gw.alipayobjects.com/zos/rmsportal/jBfVSpDwPbitsABtDDlB.svg" alt="" />}>with custom icon</Button><WhiteSpace />
+        </WingBlank>
+      </div>
     </div>
   )
 }
