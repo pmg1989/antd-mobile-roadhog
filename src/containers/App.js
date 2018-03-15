9565d591907b3442d75f766150a63a7ba028564e
pmg1989 <972401854@qq.com>
2018-03-15 15:48:43 +0800

    fix env

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,83 +1,16 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { Header } from 'components'
-import { fetchTaskList, taskAdd, taskDone, taskUndone, taskRemove, taskEdit, taskFilter } from 'actions'
-import TaskForm from './Todo/TaskForm'
-import TaskList from './Todo/TaskList'
-import TaskStats from './Todo/TaskStats'
-import selector from '../selectors/todo'
 
-class App extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-    tasks: PropTypes.object.isRequired,
-    taskCount: PropTypes.number.isRequired,
-    doneTaskCount: PropTypes.number.isRequired,
-    filters: PropTypes.string.isRequired,
-    onFetchTaskList: PropTypes.func.isRequired,
-    onTaskAdd: PropTypes.func.isRequired,
-    onTaskDone: PropTypes.func.isRequired,
-    onTaskUndone: PropTypes.func.isRequired,
-    onTaskRemove: PropTypes.func.isRequired,
-    onTaskEdit: PropTypes.func.isRequired,
-    onTaskFilter: PropTypes.func.isRequired,
-  }
-
-  componentDidMount () {
-    const { onFetchTaskList } = this.props
-    onFetchTaskList()
-  }
-
-  render () {
-    const { children, tasks, taskCount, doneTaskCount, filters,
-            onTaskAdd, onTaskDone, onTaskUndone, onTaskRemove, onTaskEdit, onTaskFilter,
-          } = this.props
-
-    const headerProps = {
-      leftContent: '',
-      icon: null,
-      onLeftClick () { },
-    }
-
-    const taskListProps = {
-      tasks,
-      onTaskEdit,
-      onTaskDone,
-      onTaskUndone,
-      onTaskRemove,
-    }
-
-    const taskStatsProps = {
-      filters,
-      taskCount,
-      doneTaskCount,
-      onTaskFilter,
-    }
-
-    return (
-      <div id="viewport" className="content-box">
-        <Header {...headerProps}>todo mvc</Header>
-        <div className="content">
-          <TaskForm onSave={onTaskAdd} />
-          <TaskList {...taskListProps} />
-          <TaskStats {...taskStatsProps} />
-          {children}
-        </div>
-      </div>
-    )
-  }
+const App = ({ children }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
 }
 
-const mapDispatchToProps = dispatch => ({
-  onFetchTaskList: bindActionCreators(fetchTaskList, dispatch),
-  onTaskAdd: bindActionCreators(taskAdd, dispatch),
-  onTaskDone: bindActionCreators(taskDone, dispatch),
-  onTaskUndone: bindActionCreators(taskUndone, dispatch),
-  onTaskRemove: bindActionCreators(taskRemove, dispatch),
-  onTaskEdit: bindActionCreators(taskEdit, dispatch),
-  onTaskFilter: bindActionCreators(taskFilter, dispatch),
-})
+App.propTypes = {
+  children: PropTypes.node,
+}
 
-export default connect(selector, mapDispatchToProps)(App)
+export default App
--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,38 @@
 const Routes = [
   {
     path: '/',
-    getComponent (location, cb) {
+    component: require('./containers/App'),
+    getIndexRoute (location, cb) {
       require.ensure([], (require) => {
-        cb(null, require('./containers/App'))
+        cb(null, { component: require('./containers/Todo') })
       }, 'home')
     },
-  },
-  {
-    path: 'demo1',
-    getComponent (location, cb) {
-      require.ensure([], (require) => {
-        cb(null, require('./containers/Demo'))
-      }, 'demo1')
-    },
-  },
-  {
-    path: 'demo2',
-    getComponent (location, cb) {
-      require.ensure([], (require) => {
-        cb(null, require('./containers/Demo2'))
-      }, 'demo2')
-    },
-  },
-  {
-    path: 'demo3',
-    getComponent (location, cb) {
-      require.ensure([], (require) => {
-        cb(null, require('./containers/Demo3'))
-      }, 'demo3')
-    },
+    childRoutes: [
+      {
+        path: 'demo1',
+        getComponent (location, cb) {
+          require.ensure([], (require) => {
+            cb(null, require('./containers/Demo'))
+          }, 'demo1')
+        },
+      },
+      {
+        path: 'demo2',
+        getComponent (location, cb) {
+          require.ensure([], (require) => {
+            cb(null, require('./containers/Demo2'))
+          }, 'demo2')
+        },
+      },
+      {
+        path: 'demo3',
+        getComponent (location, cb) {
+          require.ensure([], (require) => {
+            cb(null, require('./containers/Demo3'))
+          }, 'demo3')
+        },
+      },
+    ],
   },
   {
     path: '*',
