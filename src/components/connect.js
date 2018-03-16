f259fffef2d63f5e7a25ea8bf61fcf14d66bfb9e
pmg1989 <972401854@qq.com>
2018-03-16 18:54:05 +0800

    fix env

--- src/components/connect.js
+++ src/components/connect.js
@@ -1,9 +1,13 @@
 import { connect } from 'react-redux'
-import { hot } from 'react-hot-loader'
-import { compose } from 'redux'
+import { CLIENT_ENV } from 'utils/config'
 
 const myConnect = (state, dispatch) => myModule => (WrappedComponent) => {
-  return compose(connect(state, dispatch), hot(myModule))(WrappedComponent)
+  if (CLIENT_ENV === 'development') {
+    const { hot } = require('react-hot-loader')
+    const { compose } = require('redux')
+    return compose(connect(state, dispatch), hot(myModule))(WrappedComponent)
+  }
+  return connect(state, dispatch)(WrappedComponent)
 }
 
 export default myConnect
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { hot } from 'react-hot-loader'
 
 const App = ({ children }) => {
   return (
@@ -14,4 +13,4 @@ App.propTypes = {
   children: PropTypes.node,
 }
 
-export default hot(module)(App)
+export default App
