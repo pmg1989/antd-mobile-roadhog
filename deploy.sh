eef8b4c265611b7ec18669cbf8beef0e91e26f51
pmg1989 <972401854@qq.com>
2018-03-26 15:50:28 +0800

    fix docker env

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ docker stop nginx-antd-mobile-roadhog
 docker rm nginx-antd-mobile-roadhog
 docker build -t nginx/antd-mobile-roadhog .
 docker run --name nginx-antd-mobile-roadhog \
--d -p 80:80 \
+-d -p 8888:80 \
 -v "$PWD"/dist:/usr/share/nginx/html \
 nginx/antd-mobile-roadhog
