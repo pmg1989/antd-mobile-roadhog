be197e8d80b1b0002a97c32efc23a72757bcfe72
pmg1989 <972401854@qq.com>
2018-03-27 12:34:52 +0800

    fix service-worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const webpack = require('webpack')
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 const { zhugeAppKey } = require('./src/utils/config')
 
 module.exports = (webpackConfig, env) => {
@@ -30,6 +31,11 @@ module.exports = (webpackConfig, env) => {
       } : null,
       hash: true,
       zhugeAppKey,
+    }),
+    new SWPrecacheWebpackPlugin({
+      // minify: true,
+      cacheId: 'antd-mobile-roadhog',
+      filename: 'service-worker.js',
     }),
   ])
 
